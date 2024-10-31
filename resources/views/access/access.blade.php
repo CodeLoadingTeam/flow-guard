c5708c3f878af7b8ecaf7d84d9925d91871f1dd5
bruno.jose13 <bruninhosese12@gmail.com>
2024-10-31 16:12:39 -0300

    chore: code style

--- resources/views/access/access.blade.php
+++ resources/views/access/access.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
 
 
 </head>
+
 <body>
     <!-- Sessão 1  -->
     <section id="section1" class="visible">
@@ -17,19 +19,19 @@
             <div class="box">
                 <h3 id="tipo_placa">Selecione o tipo da placa:</h3>
                 <!-- <form action="{{ route('automobile.enter') }}" method="POST"> -->
-                    <div>
-                        <input type="radio"id="mercosul"name="plate"value="placa_mercosul">
-                        <label for="mercosul">Mercosul</label><br>
-                    </div>
-                    <div class="rd_placa_antiga">
-                        <input type="radio" id="old" name="plate" value="placa_antiga">
-                        <label for="old">Placa antiga.</label> 
-                    </div>
+                <div>
+                    <input type="radio"id="mercosul" name="plate" value="placa_mercosul">
+                    <label for="mercosul">Mercosul</label><br>
+                </div>
+                <div class="rd_placa_antiga">
+                    <input type="radio" id="old" name="plate" value="placa_antiga">
+                    <label for="old">Placa antiga.</label>
+                </div>
 
-                    <h3>Insira a Placa:</h3>
+                <h3>Insira a Placa:</h3>
 
-                    <input type="text" name="plate"><br><br>
-                    <button id="entrarButton">Entrar</button>
+                <input type="text" name="plate"><br><br>
+                <button id="entrarButton">Entrar</button>
                 </form>
             </div>
         </div>
@@ -40,26 +42,26 @@
         <!-- <div class="container">
             <div class="header">
                 <h3>Section 2</h3> -->
-            </div>
-            <div class="content">
-                <div class="logo">LOGO</div>
-                <div class="user-info">
-                    <img src="img/user_icon.png" alt="Ícone de Usuário" class="user-icon">
-                    <div class="user-details">
-                        <p><strong>Características do morador:</strong></p>
-                        <p>Nome: André Caismiro </p>
-                        <p>APTO: 03</p>
-                        <p>Bloco: 2 </p>
-                    </div>
-                </div>
-                <div class="car-info">
-                    <p><strong>Características do carro:</strong></p>
-                    <p>Marca: Ford </p>
-                    <p>Modelo: Escort</p>
-                    <p>Placa: CHS6D83 - Mercosul</p>
-                    <p>Data/Horário: 17/10/2024 - 22:15 </p>
+        </div>
+        <div class="content">
+            <div class="logo">LOGO</div>
+            <div class="user-info">
+                <img src="img/user_icon.png" alt="Ícone de Usuário" class="user-icon">
+                <div class="user-details">
+                    <p><strong>Características do morador:</strong></p>
+                    <p>Nome: André Caismiro </p>
+                    <p>APTO: 03</p>
+                    <p>Bloco: 2 </p>
                 </div>
             </div>
+            <div class="car-info">
+                <p><strong>Características do carro:</strong></p>
+                <p>Marca: Ford </p>
+                <p>Modelo: Escort</p>
+                <p>Placa: CHS6D83 - Mercosul</p>
+                <p>Data/Horário: 17/10/2024 - 22:15 </p>
+            </div>
+        </div>
         </div>
     </section>
 
@@ -67,4 +69,5 @@
 
     <script src="js/access/script.js"></script>
 </body>
+
 </html>
